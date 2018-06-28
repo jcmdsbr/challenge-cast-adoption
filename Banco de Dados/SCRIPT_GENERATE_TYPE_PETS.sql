04b14d9a031e3f2927fb1c4600c38457c2c4b8f8
Jean Carlos Moreira da Silva <33285182+jcmds@users.noreply.github.com>
2018-06-28 15:53:06 -0300

    Rename SCRIPT_ALIMENTAÇÃO_TIPO_ANIMAL.sql to SCRIPT_GENERATE_TYPE_PETS.sql

---
+++ Banco de Dados/SCRIPT_GENERATE_TYPE_PETS.sql
@@ -0,0 +1,6 @@
+﻿USE [SGA]
+GO
+INSERT [dbo].[tipo_animal] ([cd_tipo_animal], [dc_tipo_animal]) VALUES (N'5fff564f-62ed-48b6-ac58-348b501c7b38', N'Hamster')
+INSERT [dbo].[tipo_animal] ([cd_tipo_animal], [dc_tipo_animal]) VALUES (N'1f1d76a7-ccfb-48b0-b924-5b71ebdf8c1d', N'Coelho')
+INSERT [dbo].[tipo_animal] ([cd_tipo_animal], [dc_tipo_animal]) VALUES (N'cb497ef0-1d12-4426-8238-76ac613ac0d1', N'Cachorro')
+INSERT [dbo].[tipo_animal] ([cd_tipo_animal], [dc_tipo_animal]) VALUES (N'1a34c3cd-7c4e-4cbe-8101-871fab84d817', N'Gato')
